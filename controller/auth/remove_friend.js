359a5c76862b1bf4edda97dde180400765a965a5
adamsyy <adamoommenjacob@gmail.com>
2022-07-02 15:50:38 +0530

    .

--- controller/auth/remove_friend.js
+++ controller/auth/remove_friend.js
@@ -10,6 +10,12 @@ module.exports.remove_friend = async (req, res) => {
    user[0]["friends"]=friends.remove(req.body.friend);
    user[0]["followers"]=user[0]["followers"]-1;
     await user[0].save();
+    var followed_Friend = await Userschema.findOne({ name: req.body.friend });
+    if(followed_Friend["followers"]!=0){
+        followed_Friend["followers"] = followed_Friend["followers"] - 1;
+        followed_Friend.save();
+    }
+   
 
     //add names to user.friends
    // var friend=req.body.friend;
