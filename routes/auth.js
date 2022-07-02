d467179c26bcc1e90ceb4a0c0018404c8815c33d
adamsyy <adamoommenjacob@gmail.com>
2022-07-02 20:39:50 +0530

    friends

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ const {show_everyone}=require("../controller/auth/show_everyone")
 const {specific_user}=require("../controller/auth/specific_user")
 const {specific_user2}=require("../controller/auth/specific_user2")
 const {test}=require("../controller/auth/test")
+const {friends}=require("../controller/auth/friends")
 const router = require("express").Router();
 
 router.post("/signup",signup)
@@ -21,6 +22,7 @@ router.post("/show_people",show_people)
 router.get("/show_everyone",show_everyone)
 router.post("/specific_user",specific_user)
 router.post("/specific_user2",specific_user2)
+router.post("/friends",friends)
 
 
 
