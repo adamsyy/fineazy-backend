73c01f441d49e3ac321541b61a2e32002000c191
adamsyy <adamoommenjacob@gmail.com>
2022-07-02 09:40:09 +0530

    added firnds

--- controller/auth/add_friend.js
+++ controller/auth/add_friend.js
@@ -4,20 +4,30 @@ const Userschema = require("../../model/user");
 
 
 module.exports.add_friend = async (req, res) => {
-
+try{
     var user=await Userschema.find({email:req.body.email});
- var friends=user[0].friends;
-   user[0]["friends"]=friends.concat(req.body.friend);
-   user[0]["followers"]=user[0]["followers"]+1;
+    var friends=user[0].friends;
+    //if friend is not in friends
+    if(friends.indexOf(req.body.friend)===-1){
+    user[0]["friends"]=friends.concat(req.body.friend);
+    user[0]["followers"]=user[0]["followers"]+1;
     await user[0].save();
+    }
+ 
+   
+       //add names to user.friends
+      // var friend=req.body.friend;
+       //add friend to user.friends
+   //const x=user.friends
+       // user.friends=[...user.friends,friend];
+       // await user.save();
+   res.send(user)
 
-    //add names to user.friends
-   // var friend=req.body.friend;
-    //add friend to user.friends
-//const x=user.friends
-    // user.friends=[...user.friends,friend];
-    // await user.save();
-res.send(user)
+
+}catch(e){
+    res.status(500).send("invalid mail id probably :)");
+}
+   
 
     
 }
