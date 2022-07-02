da42ad8805551baf0261a58c7b666c4eda3bdf92
adamsyy <adamoommenjacob@gmail.com>
2022-07-02 15:46:49 +0530

    .

--- controller/basket/users_portfolio.js
+++ controller/basket/users_portfolio.js
@@ -13,7 +13,7 @@ try{
     //   apiSecret: "JmU2M67eYD9ZqBLMF8JH4R5XPpK8owoZHjkFcnNRFiIyVed87aVh5VuQmx7FHjC8",
     //      getTime:()=> new client.time()
     //   })
-
+//
       const client = Binance({
         apiKey:user.apikey,
         apiSecret: user.apisecret,
